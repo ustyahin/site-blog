94391b126a3276a1f246c4c960bca7e3630bed39
egor <ustyahin.egor@gmail.com>
2023-10-08 18:33:05 +0700

    Правки главной страницы: добавлено отображение ошибок, правки работы с базой и постами

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
-include("class.php");
+
+require "Database.php";
+require "Post.php";
+
 if (isset($_COOKIE['user_login'])) {
     header( 'Location: /feed.php');
 }
+
+$database = new Database();
+$database->connect();
+
 ?>
 
 <!DOCTYPE html>
@@ -20,15 +31,42 @@ if (isset($_COOKIE['user_login'])) {
         
         <div id="sidebar">
             <p><a href="/sign.php">Войти на сайт</a></p>
-            <p><a href="register.php">Зарегистрироваться</a></p>
-            <p><h3>Категории:</h3></p>
+            <p><a href="/register.php">Зарегистрироваться</a></p>
+            <p><h3>Категории:</h3>
+            <ul>
+                <?php
+                // var_dump($database->getCategories());
+                ?>
+                <li></li>
+            </ul>
+
+            </p>
         </div>
 
         <div id="content">
             <?php
-            $show_all_posts = new Database();
-            $show_all_posts->connect();
-            $show_all_posts->show_all_posts();
+            $post = new Post($database);
+            $posts = $post->getAll();
+
+            foreach ($posts as $post) {
+                echo
+                    "<div class='card'>".
+                    "<table border='1'>".
+                    "<tbody>";
+                echo '<h1>' . $post['title'] . '</h1>';
+                echo '<p>' . $post['text'] . '</p>';
+
+                    // "<tr><td align='center'>Категория: ".$assoc['category']."</td><td align='center'>Автор: ".$assoc['login']."</td></tr>".
+                    // "<tr><td align='center' colspan='2'>".$assoc['title']."</td></tr>".
+                    // "<tr><td align='center' colspan='2'>".$assoc['text']."</td></tr>".
+                    // "<tr><td align='center'>".$assoc['date']."</td><td>".$assoc['date_changed']."</td></tr>".
+
+                    echo "</tbody>".
+                    "</table>".
+                    "</div>".
+                    "<br />";
+
+            }
             ?>
         </div>      
         
